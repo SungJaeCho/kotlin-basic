a0b8c61504d70cf2c8d8f66c9bc0d1e6e9db478c
liadus <ridiss1224@gmail.com>
2022-06-30 08:31:09 +0900

    클래스의 다형성 is 캐스팅 :  if문안에서만 잠시 다운캐스팅됨 as 캐스팅 : 그냥 다운캐스팅 처리함, 캐스팅 당하는 변수도 다운캐스팅처리됨

--- src/test/kotlin/com/study/kotlin01/grammar/GrammarTestCasting01.kt
+++ src/test/kotlin/com/study/kotlin01/grammar/GrammarTestCasting01.kt
@@ -7,28 +7,35 @@ class GrammarTestCasting01 {
     @Test
     fun `캐스팅테스트`() {
         var a = Drinks()
-        var b = Cola()
+        a.drink()
 
-        if (b is Cola) {
-            println("성공")
+        var b:Drinks = Cola()
+        b.drink()
+//        b.washDishes() 는 호출이 불가하므로 downCasting이 필요함
+        if (b is Cola) { // is 캐스팅의 경우 if문안에서만 잠시 downCasting됨
+            b.washDishes()
         }
-        var c  = a as Cola
+        // as는 그냥 downCastring 처리함
+        var c = b as Cola
+        c.washDishes()
+        b.washDishes() //아까와는 달리 오류가 안나는데 as 캐스팅시 b 변수자체도 다운캐스팅 됨
+
     }
 
     open class Drinks{
         var name: String = "음료수"
         open fun drink() {
-            println("${name}을 마십니다.")
+            println("${name}를 마십니다.")
         }
     }
 
     class Cola : Drinks() {
         var type: String = "콜라"
         override fun drink() {
-            println("${name}인 ${type}을 마십니다.")
+            println("${name}중 ${type}을(를) 마십니다.")
         }
-        fun taste() {
-            println("${type}향이 납니다.")
+        fun washDishes() {
+            println("${type}로 설거지를 합니다.")
         }
     }
 }
